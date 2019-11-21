71ae123452143bef6f7b59ddc5e3ccb1580a5287
fancyyan123 <2589412242@qq.com>
2019-11-21 19:43:04 +0800

    add some features extraction method

--- models/feature_engineering.py
+++ models/feature_engineering.py
@@ -1,70 +1,9 @@
 from common import log, timeit
+from models.feature_common import _pos_tag, _gen_tfidf_matrix, _type_token_ratio
 import re
 from functools import reduce
 import pandas as pd
-import numpy as np
-from pandas import Series, DataFrame
-import math
-
-
-def _clean_en_text(text_with_index, re_space, stopwords):
-    text, index = text_with_index
-    text = re_space.sub(' ', text).lower()
-
-    filtered_words = text.split(' ')
-    filtered_words = list(map(lambda word: word.strip(), filtered_words))
-    if stopwords:
-        filtered_words = [w for w in filtered_words if w.strip() not in stopwords]
-    return ' '.join(filtered_words), index
-
-
-def _word_freq(essay_words):
-    word_freq = {}
-    for each in essay_words:
-        if each in word_freq:
-            word_freq[each] += 1
-        else:
-            word_freq[each] = 1
-    return word_freq
-
-
-def _gen_tfidf_matrix(data, vector_length, stopwords):
-    re_space = re.compile(r'[\s+\?\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+')
-    clean_data = dict()
-    # 去停词，去标点符号
-    for i, text in data.items():
-        text, i = _clean_en_text((text, i), re_space, stopwords)
-        clean_data[i] = text
-        
-    # word_freq统计所有单词的词频, word_docs统计每个单词在多少文档中出现过
-    word_freq, word_docs = dict(), dict()
-    for (i, text) in clean_data.items():
-        text_words = list(filter(lambda x: x!='', text.split(' ')))
-        essay_word_freq = _word_freq(text_words)
-        for item in essay_word_freq.items():
-            if item[0] in word_freq:
-                word_freq[item[0]] += item[1]
-            else:
-                word_freq[item[0]] = item[1]
-            if item[0] in word_docs:
-                word_docs[item[0]] += 1
-            else:
-                word_docs[item[0]] = 1
-    word_freq_sorted = sorted(word_freq.items(), key=lambda x: x[1], reverse=True)
-    words_select = [each[0] for each in word_freq_sorted[:vector_length]]
-    doc_freq = {k: word_docs[k] for k in words_select}
-
-    tfidf = dict()
-    for (i, text) in clean_data.items():
-        text_words = list(filter(lambda x: x != '', text.split(' ')))
-        vector = np.zeros(vector_length)
-        essay_word_freq = _word_freq(text_words)
-        for index in range(len(words_select)):
-            word = words_select[index]
-            if word in essay_word_freq:
-                vector[index] = essay_word_freq[word]*math.log(len(data)/doc_freq[word], 10)
-        tfidf[i] = vector
-    return Series(tfidf)
+import nltk
 
 
 # TODO: 分词、去除停用词？、序列化、提取句法特征、提取词法特征(tf-idf)、提取语义特征
@@ -119,17 +58,30 @@ class TaskIndependentFeatureEngineer(FeatureEngineer):
 
     # the occurance of commas, quotations and exclamation marks
     def _occurrence_feature(self, essay):
-        pass
+        return essay.count(','), essay.count('!'), essay.count("\'")+essay.count("\"")
 
     # syntax features:
     # measuring the ratio of distinct parse trees to
     # all the trees and the average depths of the trees
+    # and measure the proportion of subordinate,
+    # causal and temporal clauses
     def _syntax_features(self, essay):
         pass
 
-    # the relative ratio of POS-tags to detect stylepreferences of writers
+    # the relative ratio of POS-tags to detect style preferences of writers
+    # together with the type token ratio to detect the diversity of vocabulary
     def _style_features(self, essay):
-        pass
+        pos_tag_list = _pos_tag(essay)
+        A, B = 0, 0
+        for each in pos_tag_list:
+            if each in []:
+                A += 1
+            if each in []:
+                B += 1
+        N = len(nltk.word_tokenize(essay))
+        formal_feature = (A/N-B/N+100)/2
+        return formal_feature, _type_token_ratio(essay)
+
 
     def _cohesion_features(self, essay):
         pass
@@ -153,3 +105,4 @@ class TaskIndependentFeatureEngineer(FeatureEngineer):
 if __name__ == "__main__":
     s = "I am a student. I love playing basketball! Do you love that?"
     #fe = TaskIndependentFeatureEngineer()
+    print(_pos_tag(s))

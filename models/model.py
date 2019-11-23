9ff018b62a4fe27b188eca58b311653bdb8db7ac
rex <xuzhuoer.rex@gmail.com>
2019-11-23 21:11:57 +0800

    Add requirement & test for model

--- models/model.py
+++ models/model.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from common import timeit
 from constant import RANDOM_STATE
 from models.classifier import Classifier
-from models.feature_engineering import FeatureEngineer
 from models.classifiers.svm_classifier import SVMClassifier
 from models.classifiers.elastic_net import ElasticNetClassifier
 
@@ -12,13 +11,11 @@ class Model(Classifier):
     def __init__(self, metadata, *args, **kwargs):
         super(Model, self).__init__(*args, **kwargs)
         self._meta = metadata
-        self._fe = FeatureEngineer()
         self._model = ElasticNetClassifier(*args, **kwargs)
 
     @timeit
     def fit(self, dataset, *args, **kwargs):
         train_data, train_label = dataset
-        train_data = self._fe.fit_transform(train_data)
         x_hyper, _, y_hyper, _ = train_test_split(
             train_data, train_label,
             test_size=0.7, random_state=RANDOM_STATE
@@ -33,5 +30,14 @@ class Model(Classifier):
 
     @timeit
     def predict(self, data, *args, **kwargs):
-        data = self._fe.transform(data)
         return self._model.predict(data, *args, **kwargs)
+
+
+if __name__ == '__main__':
+    import pickle
+    from collections import namedtuple
+    FeatureDatasets = namedtuple("FeatureDatasets", ['train', 'train_label', 'valid', 'valid_label', 'test'])
+    set_id = 1
+    with open(f'../features/SyntaxFeatureLabelSet{set_id}.pl', 'rb') as f:
+        dataset = pickle.load(f)
+    print('here')

593c4e1ec81ae325bf3d8685b76c928cce4a8638
rex <xuzhuoer.rex@gmail.com>
2019-11-24 13:31:44 +0800

    Add compare

--- models/model.py
+++ models/model.py
@@ -36,22 +36,36 @@ class Model(Classifier):
 if __name__ == '__main__':
     import pandas as pd
     import numpy as np
-    from collections import namedtuple
     from constant import ESSAY_INDEX, ESSAY_LABEL
     from ingestion.metrics import kappa
-    FeatureDatasets = namedtuple("FeatureDatasets", ['train', 'train_label', 'valid', 'valid_label', 'test'])
-    set_id = 1
-    csv_params = {
-        'index_col': ESSAY_INDEX,
-        'dtype': {'domain1_score': np.float}
-    }
-    train_data = pd.read_csv(f"../features/SyntaxFeatureLabelTrainSet{set_id}.csv", **csv_params)
-    train_label = pd.read_csv(f"../features/SyntaxFeatureLabelTrainLabel{set_id}.csv", **csv_params)
-    valid_data = pd.read_csv(f"../features/SyntaxFeatureLabelValidSet{set_id}.csv", **csv_params)
-    valid_label = pd.read_csv(f"../features/SyntaxFeatureLabelValidLabel{set_id}.csv", **csv_params)
-    test_data = pd.read_csv(f"../features/SyntaxFeatureLabelTestSet{set_id}.csv", **csv_params)
-    model = Model({})
-    model.fit((train_data, train_label))
-    y_preds = model.predict(valid_data)
-    res = kappa(valid_label[ESSAY_LABEL].tolist(), y_preds)
-    print(f"Final kappa: {res}")
+    original_kappas = [
+        0.8026615360220601,
+        0.6453843679639755,
+        0.6675004632203076,
+        0.6201412784903135,
+        0.7983501888241695,
+        0.6810744597077609,
+        0.733541110264561,
+        0.7055995750817732
+    ]
+    start = 1
+    stop = 2
+    my_kappas = []
+    for set_id in range(start, stop):
+        csv_params = {
+            'index_col': ESSAY_INDEX,
+            'dtype': {'domain1_score': np.float}
+        }
+        train_data = pd.read_csv(f"../features/SyntaxFeatureLabelTrainSet{set_id}.csv", **csv_params)
+        train_label = pd.read_csv(f"../features/SyntaxFeatureLabelTrainLabel{set_id}.csv", **csv_params)
+        valid_data = pd.read_csv(f"../features/SyntaxFeatureLabelValidSet{set_id}.csv", **csv_params)
+        valid_label = pd.read_csv(f"../features/SyntaxFeatureLabelValidLabel{set_id}.csv", **csv_params)
+        test_data = pd.read_csv(f"../features/SyntaxFeatureLabelTestSet{set_id}.csv", **csv_params)
+        model = Model({})
+        model.fit((train_data, train_label))
+        y_preds = model.predict(valid_data)
+        res = kappa(valid_label[ESSAY_LABEL].tolist(), y_preds)
+        my_kappas.append(res)
+    df = pd.DataFrame({'Baseline': original_kappas[start - 1: stop - 1], 'Our solution': my_kappas})
+    df['Improvement'] = df['Our solution'] - df['Baseline']
+    print(f"Final result:\n{df}")

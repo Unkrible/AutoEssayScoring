7a4d5db3fb634eff7b77087d452d747f87a9a266
rex <xuzhuoer.rex@gmail.com>
2019-12-27 21:20:36 +0800

    Add preprocess

--- prompt_independent/dnn_model.py
+++ prompt_independent/dnn_model.py
@@ -9,12 +9,13 @@ from models.classifier import Classifier
 
 
 class DNNModel(Classifier):
-    def __init__(self, hidden_dim, dense_dim, weight, max_words, opt="adam", **kwargs):
+    def __init__(self, hidden_dim, dense_dim,  embedding_dim, embedding_table, max_words, opt="adam", **kwargs):
         self.hidden_dim = hidden_dim
         self.dense_dim = dense_dim
 
-        self.vocab_size, self.embedding_dim = weight.shape
-        self.weights = [weight]
+        self.embedding_dim = embedding_dim
+        self.embedding_table = embedding_table
+        self.vocab_size = len(embedding_table)
 
         self.filters = 1
         self.filter_len = 1
@@ -24,53 +25,56 @@ class DNNModel(Classifier):
         self.nb_feature = 21
 
         self.opt = opt
+        self.model = self.model_essay()
         super(DNNModel, self).__init__()
 
-        # uses only the semantic network
-        def model_essay(self):
-            input_words = Input(shape=(self.max_words,), dtype='int32')
-            embedding_layer = Embedding(input_dim=self.vocab_size,
-                                        output_dim=self.embedding_dim,
-                                        weights=self.weights,
-                                        trainable=False,
-                                        mask_zero=True)(input_words)
-            bi_lstm_layer = Bidirectional(
-                LSTM(units=self.embedding_dim, output_dim=self.hidden_dim, return_sequences=False),
-                merge_mode='concat'
-            )(embedding_layer)
+    # uses only the semantic network
+    def model_essay(self):
+        input_words = Input(shape=(self.max_words,), dtype='int32')
+        embedding_layer = Embedding(input_dim=self.vocab_size,
+                                    output_dim=self.embedding_dim,
+                                    weights=self.embedding_table,
+                                    trainable=False,
+                                    mask_zero=True)(input_words)
+        bi_lstm_layer = Bidirectional(
+            LSTM(output_dim=self.hidden_dim, return_sequences=False),
+            merge_mode='concat'
+        )(embedding_layer)
 
-            sentence_model = Model(inputs=input_words, outputs=bi_lstm_layer)
+        sentence_model = Model(inputs=input_words, outputs=bi_lstm_layer)
 
-            input_essay = Input(shape=(None, self.max_words), dtype='int32')
+        input_essay = Input(shape=(None, self.max_words), dtype='int32')
 
-            essay_layer = TimeDistributed(sentence_model)(input_essay)
+        essay_layer = TimeDistributed(sentence_model)(input_essay)
 
-            essay_bilstm_layer = Bidirectional(
-                LSTM(output_dim=self.hidden_dim, return_sequences=False),
-                merge_mode='concat'
-            )(essay_layer)
+        essay_bilstm_layer = Bidirectional(
+            LSTM(output_dim=self.hidden_dim, return_sequences=False),
+            merge_mode='concat'
+        )(essay_layer)
 
-            bn_merge_layer2 = BatchNormalization()(essay_bilstm_layer)
-            merge_dense_layer2 = Dense(self.dense_dim, activation='relu')(bn_merge_layer2)
-            score_layer = Dense(1, activation='sigmoid', name='pred_score')(merge_dense_layer2)
-            essay_model = Model(inputs=input_essay, outputs=score_layer)
+        bn_merge_layer2 = BatchNormalization()(essay_bilstm_layer)
+        merge_dense_layer2 = Dense(self.dense_dim, activation='relu')(bn_merge_layer2)
+        score_layer = Dense(1, activation='sigmoid', name='pred_score')(merge_dense_layer2)
+        essay_model = Model(inputs=input_essay, outputs=score_layer)
 
-            if self.opt == "adam":
-                optimizer = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=0, clipvalue=10)
-            elif self.opt == 'rmsprop':
-                optimizer = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-06, clipnorm=0, clipvalue=10)
-            else:
-                optimizer = optimizers.Adagrad(lr=0.01, epsilon=1e-06, clipnorm=0, clipvalue=10)
+        if self.opt == "adam":
+            optimizer = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=0, clipvalue=10)
+        elif self.opt == 'rmsprop':
+            optimizer = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-06, clipnorm=0, clipvalue=10)
+        else:
+            optimizer = optimizers.Adagrad(lr=0.01, epsilon=1e-06, clipnorm=0, clipvalue=10)
 
-            essay_model.compile(optimizer=optimizer,
-                                loss="mean_squared_error",
-                                metrics=['mean_squared_error'])
-            return essay_model
+        essay_model.compile(optimizer=optimizer,
+                            loss="mean_squared_error",
+                            metrics=['mean_squared_error'])
+        return essay_model
 
     def fit(self, dataset, *args, bad_index=None, good_index=None, **kwargs):
         bad_labels = pd.Series(np.zeros(len(bad_index)), index=bad_index)
         good_labels = pd.Series(np.ones(len(good_index)), index=good_index)
         labels = pd.concat([bad_labels, good_labels])
+        data = pd.concat([dataset[bad_index], dataset[good_index]])
+        self.model.fit(data, labels)
 
     def predict(self, data, *args, **kwargs):
         pass
--- prompt_independent/main.py
+++ prompt_independent/main.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.python.keras.preprocessing import text
+from tensorflow.python.keras.preprocessing import sequence
 
 from constant import ESSAY_INDEX, ESSAY_LABEL, RANK
 from prompt_independent.rank_model import LgbRankModel
+from prompt_independent.dnn_model import DNNModel
 
 
 def read_datasets(path, set_id, read, **csv_params):
@@ -24,7 +27,70 @@ def read_label(path, id, **csv_params):
 
 
 def read_dataset(path, id, **csv_params):
-    return pd.read_csv(f"{path}{id}.csv", **csv_params)
+    if not (isinstance(id, str) and id.endswith("tsv")):
+        id = f"{id}.csv"
+    return pd.read_csv(f"{path}{id}", **csv_params)
+
+
+def read_glove():
+    # loading pretrained embedding
+    # FT_DIR = '/app/embedding'
+    GLOVE_FILE = "/home/chengfeng/glove.6B/glove.6B.300d.txt"
+    word_embeddings = {}
+    embedding_dim = 0
+
+    i = 0
+    # Match the word of each line with the rest words -> fasttext_embeddings_index
+    with open(GLOVE_FILE, 'r') as f:
+        for line in f:
+            if i > 10:
+                break
+            else:
+                i += 1
+            values = line.strip().split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embedding_dim = len(coefs)
+            word_embeddings[word] = coefs
+    print('Found %s glove word vectors.' % len(word_embeddings))
+    return word_embeddings, embedding_dim
+
+
+def sequentialize_data(train_contents):
+    MAX_VOCAB_SIZE = 200000
+    tokenizer = text.Tokenizer(num_words=MAX_VOCAB_SIZE)
+    tokenizer.fit_on_texts(train_contents)
+    x_train = tokenizer.texts_to_sequences(train_contents)
+    max_length = len(max(x_train, key=len))
+    x_train = sequence.pad_sequences(x_train, maxlen=max_length)
+    word_index = tokenizer.word_index
+    num_features = min(len(word_index) + 1, MAX_VOCAB_SIZE)
+    return x_train, word_index, num_features, tokenizer, max_length
+
+
+def lookup_embedding(word_index, embeddings_index, embedding_dim, num_features):
+    embedding_matrix = np.zeros((num_features, embedding_dim))
+
+    beyond_cnt = 0
+    hit_cnt = 0
+    oov_cnt = 0
+    missing = []
+    # beyond_num_features = []
+
+    for word, i in word_index.items():
+        if i >= num_features:
+            beyond_cnt += 1
+            # beyond_num_features.append((word, i))
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+            hit_cnt += 1
+        else:
+            embedding_matrix[i] = np.random.uniform(-1, 1, size=embedding_dim)
+            missing.append(i)
+            oov_cnt += 1
+    return embedding_matrix
 
 
 if __name__ == '__main__':
@@ -43,7 +109,7 @@ if __name__ == '__main__':
     test_data = read_dataset(f"../{feature_file}/TestSet", set_id, **csv_params)
     test_label = read_label(f"../{feature_file}/TestLabel", set_id, **csv_params)
 
-    # train model
+    # train rank model
     rank_model = LgbRankModel()
     rank_model.fit((train_data, train_label))
     rank_hat = rank_model.predict(test_data)
@@ -51,4 +117,22 @@ if __name__ == '__main__':
     sort_index = test_data[RANK].sort_values().index
     bad_index = sort_index[:int(len(sort_index) * 0.4)]
     good_index = sort_index[int(len(sort_index) * 0.7):]
+
+    # preprocess test data
+    csv_params['sep'] = '\t'
+    test_data = read_dataset(f"../essay_data/", "test.tsv", **csv_params)
+    test_data = test_data[test_data["essay_set"] == set_id]["essay"]
+    # train dnn model
+    weights, embedding_dim = read_glove()
+    x_all, word_index, num_features, tokenizer, max_length = sequentialize_data(test_data)
+    embedding_table = lookup_embedding(word_index, weights, embedding_dim, num_features)
+    dnn_model = DNNModel(
+        hidden_dim=256,
+        dense_dim=128,
+        embedding_table=embedding_table,
+        embedding_dim=embedding_dim,
+        max_words=200000
+    )
+    dnn_model.fit(x_all, bad_index=bad_index, good_index=good_index)
+    y_all_hat = dnn_model.predict(x_all)
     print("here")
